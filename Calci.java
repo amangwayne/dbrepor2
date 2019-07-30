5843e30994ed7af6ed87738c6cf8ff71b170cbf0
Aman <amangwayne@gmail.com>
2019-07-30 12:20:58 +0530

    Initial Commit

---
+++ Calci.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class Calci
+{
+	public static int add(int x, int y)
+	{
+		return (x+y);
+	}
+	public static int subtract(int x, int y)
+	{
+		return (x-y);
+	}
+	public static int multiply(int x, int y)
+	{
+		return (x*y);
+	}
+	public static int divide(int x, int y)
+	{
+		return (x/y);
+	}
+
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int x,y,choice;
+		System.out.println("Enter two numbers");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		System.out.println("Select an operation:\n 1. Add\n 2. Subtract\n 3. Multiply\n 4. 			Divide");
+		choice = sc.nextInt();
+		switch(choice)
+		{
+			case 1: add(x,y);
+				break;
+			case 2: subtract(x,y);
+				break;
+			case 3: multiply(x,y);
+				break;
+			case 4: divide(x,y);
+				break;
+			default: System.out.println("Invalid Choice");
+		}
+	}
+}
